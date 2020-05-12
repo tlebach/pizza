bb7c84edc213c4e90b405ed66d484f9b577276d5
Tung LE BACH <bachtung1505@gmail.com>
2020-05-12 17:48:27 +0700

    Save order to DB - missing new files

---
+++ database/migrations/2020_05_11_032735_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('status');
+            $table->char('fullname');
+            $table->char('address');
+            $table->char('phone');
+            $table->char('email');
+            $table->float('total');
+            $table->float('total_inc_ship');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
